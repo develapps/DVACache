06052d4d3fca5d1b4bb6c4aa4340eb4abc5cf7e1
Pablo Romeu <pablo.romeu@develapps.com>
2015-07-21 08:43:46 +0200

    Initial Commit

---
+++ DVACacheTests/DVACacheTests.m
@@ -0,0 +1,40 @@
+//
+//  DVACacheTests.m
+//  DVACacheTests
+//
+//  Created by Pablo Romeu on 21/7/15.
+//  Copyright (c) 2015 Pablo Romeu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DVACacheTests : XCTestCase
+
+@end
+
+@implementation DVACacheTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
